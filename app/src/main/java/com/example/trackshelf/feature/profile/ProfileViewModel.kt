e368f091b121d6d184541d306eea6f263ca8eec1
wakoliVotes <votesdw@yahoo.com>
2026-07-06 15:24:43 +0300

    Profile screen viewmodel

--- app/src/main/java/com/example/trackshelf/feature/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/trackshelf/feature/profile/ProfileViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.trackshelf.feature.profile
 
-import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,17 +10,43 @@ class ProfileViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(ProfileUiState())
     val uiState: StateFlow<ProfileUiState> = _uiState.asStateFlow()
 
-    fun onEditProfileClick() {
-        // Handle edit profile click
+    fun onDarkModeChanged(darkMode: Boolean) {
+        _uiState.value = _uiState.value.copy(darkMode = darkMode)
     }
 
-    fun onLogoutClick() {
-        // Handle logout click
+    fun onNotificationsChanged(notifications: Boolean) {
+        _uiState.value = _uiState.value.copy(notifications = notifications)
     }
+
+    fun onCurrencyChanged(currency: String) {
+        _uiState.value = _uiState.value.copy(currency = currency)
+    }
+
+    fun onProfilePictureChanged(profilePictureUrl: String) {
+        _uiState.value = _uiState.value.copy(profilePictureUrl = profilePictureUrl)
+    }
+
+    fun onNameChanged(name: String) {
+        _uiState.value = _uiState.value.copy(name = name)
+    }
+
+    fun onEmailChanged(email: String) {
+        _uiState.value = _uiState.value.copy(email = email)
+    }
+
+
 }
 
 annotation class ProfileUiState (
     val name: String = "John Doe",
     val email: String = "john.doe@example.com",
     val profilePictureUrl: String = "https://example.com/profile.jpg",
+    val aboutUs: String = "About Us",
+    val contactUs: String = "Contact Us",
+    val social_fb: String = "Facebook",
+    val social_ig: String = "Instagram",
+    val social_tw: String = "Twitter",
+    val social_yt: String = "YouTube",
+    val social_li: String = "LinkedIn",
+    val social_wa: String = "WhatsApp"
 )
