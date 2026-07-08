39f5406decb1aed9b3047096e29ec723ba552104
wakoliVotes <votesdw@yahoo.com>
2026-07-08 22:21:56 +0300

    2026-07-08 22:21:56

--- app/src/main/java/com/example/trackshelf/feature/sales/SalesViewModel.kt
+++ app/src/main/java/com/example/trackshelf/feature/sales/SalesViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.trackshelf.feature.sales
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+class SalesViewModel: ViewModel () {
+    private val _uiState = MutableStateFlow(SalesUiState())
+    val uiState: StateFlow<SalesUiState> = _uiState.asStateFlow()
+
+
+}
+
+annotation class SalesUiState
+
